df2a2d8c99926afd19baab90289c9b2be939e0e0
judemin <judemin2087@naver.com>
2021-09-07 18:38:12 +0900

    update : update payment function

--- src/pages/Payment.js
+++ src/pages/Payment.js
@@ -5,10 +5,11 @@ const Payment = () => {
 
     function startPayment(e){
         e.preventDefault();
+        
         BootPay.request({
             price: '1000', //실제 결제되는 가격
             application_id: "61176c377b5ba4001d52a082", // ApplicationID
-            name: '슈웅 결제 테스트', //결제창에서 보여질 이름
+            name: '퍼플슈즈 결제 테스트', //결제창에서 보여질 이름
             pg: 'inicis',
             method: 'card', //결제수단, 입력하지 않으면 결제수단 선택부터 화면이 시작합니다.
             show_agree_window: 0, // 부트페이 정보 동의 창 보이기 여부
